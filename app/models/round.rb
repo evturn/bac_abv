f4e5384dbc9f2e69f7ab255e425c3a52f5e8014c
Evan <evturn@gmail.com>
2014-12-09 05:59:11 -0500

    cracked the BAC calculator code

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,15 +2,31 @@ class Round < ActiveRecord::Base
 
   def calculate(abv, lbs, sex, hours)
 
+    # A 12 fl_oz glass of .05 ABV beer
+    # A 5 fl_oz glass of  .12 ABV wine
+    # A 1.5 fl_oz glass of .40 ABV spirit
 
-    step_1 = (fl_oz * 5.14)
-    step_2 = (lbs * sex)
-    step_3 = (step_1 / step_2)
-    step_4 = (0.015 * hours)
-    step_5 = (step_3 - step_4)
+    # all contain 0.6 fl_oz of alcohol
+
+    # 3 beers (1.8 fl_oz)
+    # 155 lbs
+    # male
+    # 1 hour
+
+    fl_oz = num_beer * 0.6
+
+    step_1 = (fl_oz * 5.14) # 9.25
+    step_2 = (lbs * sex)  # 113.5
+    step_3 = (step_1 / step_2) # 0.08
+    step_4 = (0.015 * hours) # 0.015
+    step_5 = (step_3 - step_4) # 0.06
+
+
+
 
     # r = sex == "male" ? 0.73 : 0.66
     # bac = ((abv * 5.14) / (lbs * r)) - (0.015 * hours)
+
 
 
 
