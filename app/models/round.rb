056b4e8f228e1888445039820761208e5ac0bd8c
Evan <evturn@gmail.com>
2014-12-09 06:03:32 -0500

    example for BAC Calculator commented out

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,15 +2,15 @@ class Round < ActiveRecord::Base
 
   def calculate(abv, lbs, sex, hours)
 
+    # The following all contain 0.6 fl_oz of alcohol
     # A 12 fl_oz glass of .05 ABV beer
     # A 5 fl_oz glass of  .12 ABV wine
     # A 1.5 fl_oz glass of .40 ABV spirit
 
-    # all contain 0.6 fl_oz of alcohol
-
-    # 3 beers (1.8 fl_oz)
+    # *** Example ***
+    # 1.8 fl_oz (3 beers)
     # 155 lbs
-    # male
+    # 0.73 (male)
     # 1 hour
 
     fl_oz = num_beer * 0.6
@@ -19,7 +19,7 @@ class Round < ActiveRecord::Base
     step_2 = (lbs * sex)  # 113.5
     step_3 = (step_1 / step_2) # 0.08
     step_4 = (0.015 * hours) # 0.015
-    step_5 = (step_3 - step_4) # 0.06
+    bac    = (step_3 - step_4) # 0.06
 
 
 
