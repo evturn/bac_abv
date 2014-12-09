a40a222e032d8f7c4d164d5d033a0bfc471651e5
Evan <evturn@gmail.com>
2014-12-08 21:28:28 -0500

    possible equations entered for BAC calculator in Round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,2 +1,19 @@
 class Round < ActiveRecord::Base
+
+def calculate(abv, lbs, sex, hours)
+
+
+  step_1 = (fl_oz * 5.14)
+  step_2 = (lbs * sex)
+  step_3 = (step_1 / step_2)
+  step_4 = (0.015 * hours)
+  step_5 = (step_3 - step_4)
+
+  # r = sex == "male" ? 0.73 : 0.66
+  # bac = ((abv * 5.14) / (lbs * r)) - (0.015 * hours)
+
+
+
+  end
+
 end
