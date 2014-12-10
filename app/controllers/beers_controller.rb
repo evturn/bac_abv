649c22f7c97760affa2b99f23eea5a93d0815998
Evan <evturn@gmail.com>
2014-12-09 22:08:31 -0500

    adding trending to index

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,6 +2,22 @@ class BeersController < ApplicationController
 
   def index
 
+    trend_url = "http://api.untappd.com/v4/beer/trending?client_id=#{ENV['UNTAPPD_ID']}&client_secret=#{ENV['UNTAPPD_SECRET']}"
+    
+    trending    = HTTParty.get( trend_url )
+    
+    featured    = trending['response']["micro"]["items"].sample
+
+    @f_beer     = featured['beer']['beer_name']
+    @f_label    = featured['beer']['beer_label']
+    @f_abv      = featured['beer']['beer_abv']
+    @f_brewery  = featured['brewery']['brewery_name']
+    city        = featured['brewery']['location']['brewery_city']
+    state       = featured['brewery']['location']['brewery_state']
+    country     = featured['brewery']['country_name']
+    @f_location = "#{city}, #{state} #{country}"
+
+    @top_beers  = trending['response']["micro"]["items"].map { |beer| beer['beer'] }
 
   end
 
