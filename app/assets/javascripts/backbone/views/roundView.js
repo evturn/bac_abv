ac1b78dc9cf1bf8351ac513a4b985db59189022c
Evan <evturn@gmail.com>
2014-12-11 16:07:57 -0500

    bac calculator almost done

--- app/assets/javascripts/backbone/views/roundView.js
+++ app/assets/javascripts/backbone/views/roundView.js
@@ -2,5 +2,11 @@ console.log('we got roundView');
 
 var RoundView = Backbone.View.extend({
 	template: _.template($("#bac-template").html()),
-
+	initialize: function(){
+    this.listenTo(this.model, 'change', this.render);
+  },
+	render: function(){
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
 });
