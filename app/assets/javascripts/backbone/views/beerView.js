218d78f6bbdb48dfbb5e887253eb232c45a1ac5a
Evan <evturn@gmail.com>
2014-12-11 11:10:25 -0500

    beer query renders div on page with backbone views through an ajax request

--- app/assets/javascripts/backbone/views/beerView.js
+++ app/assets/javascripts/backbone/views/beerView.js
@@ -1,14 +1,13 @@
 console.log('we got beerView');
 
 var BeerView = Backbone.View.extend({
-	tagName: "div",
-	template: _.template($("#beer_results-template").html()),
+	template: _.template($("#beer-results-template").html()),
 	initialize: function(){
     this.listenTo(this.model, 'change', this.render);
   },
 	render: function(){
-		this.$el.html(this.template(this.model));
+		this.$el.html(this.template(this.model.toJSON()));
 		return this;
-	},
+	}
 });
 
