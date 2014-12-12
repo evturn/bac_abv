54c6430375c5af7fe92f2c289b68b1070c7173fc
Evan <evturn@gmail.com>
2014-12-12 18:53:53 -0500

    template and input to be created have been built

--- app/assets/javascripts/backbone/views/addDrinkView.js
+++ app/assets/javascripts/backbone/views/addDrinkView.js
@@ -1,3 +1,12 @@
 console.log('we got drinkView')
 
-var AddDrinkView = Backbone.View.extend({});
+var AddDrinkView = Backbone.View.extend({
+	template: _.template($("#add-drink-template").html()),
+	initialize: function(){
+    this.listenTo(this.model, 'change', this.render);
+  },
+  render: function(){
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
