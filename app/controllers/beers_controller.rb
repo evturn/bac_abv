6454d38c2816c884d9ff25d2833993ed3869d987
Evan <evturn@gmail.com>
2014-12-14 19:06:57 -0500

    json names put into underscore templates

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,24 +5,25 @@ class BeersController < ApplicationController
     trend_url = "http://api.untappd.com/v4/beer/trending?client_id=#{ENV['UNTAPPD_ID']}&client_secret=#{ENV['UNTAPPD_SECRET']}" 
     trending    = HTTParty.get( trend_url )
     featured    = trending['response']["micro"]["items"].sample
-    @f_beer     = featured['beer']
-    @f_brewery  = featured['brewery']
-    render :json @f_beer.to_json
-    render :json @f_brewery.to_json
-
-    
-    
-  
-    
-
-    @f_beer     = featured['beer']['beer_name']
-    @f_label    = featured['beer']['beer_label']
-    @f_abv      = featured['beer']['beer_abv']
-    @f_brewery  = featured['brewery']['brewery_name']
     city        = featured['brewery']['location']['brewery_city']
     state       = featured['brewery']['location']['brewery_state']
     country     = featured['brewery']['country_name']
+    @f_beer     = featured['beer']
+    @f_brewery  = featured['brewery']
     @f_location = "#{city}, #{state} #{country}"
+    render :json @f_beer.to_json
+    render :json @f_brewery.to_json
+    render :json @f_location.to_json
+   
+
+    @_beer     = featured['beer']['beer_name']
+    @_label    = featured['beer']['beer_label']
+    @_abv      = featured['beer']['beer_abv']
+    @_brewery  = featured['brewery']['brewery_name']
+    _city        = featured['brewery']['location']['brewery_city']
+    _state       = featured['brewery']['location']['brewery_state']
+    _country     = featured['brewery']['country_name']
+    @_f_location = "#{city}, #{state} #{country}"
 
     @top_beers = trending['response']["micro"]["items"].map { |item| 
         item }
