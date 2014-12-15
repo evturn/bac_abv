b32781365947e196b0b538ab314e412cc744a6f9
Evan <evturn@gmail.com>
2014-12-14 23:06:26 -0500

    featured shows up on page, json may need to be parsed better for readability

--- app/assets/javascripts/backbone/models/featuredBeer.js
+++ app/assets/javascripts/backbone/models/featuredBeer.js
@@ -2,14 +2,18 @@ console.log('we got featuredBeer');
 
 var FeaturedBeer = Backbone.Model.extend({
 	defaults: {
-	name: "beer_name",
-	abv: "beer_name",
-	style: "beer_style",
-	label: "beer_label",
-	brewery: "brewery_name",
-	city: "brewery_city",
-	state: "brewery_state",
-	country: "country_name",
+		beer: {
+			name: "beer_name",
+			beer_abv: "",
+			style: "",
+			label: "",
+		},
+		brewery: {
+			brewery: "",
+			city: "",
+			state: "",
+			country: ""
+		}
 	},
 	url: '/featured',
 	render: function() {
