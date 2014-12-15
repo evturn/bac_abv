50fc4e94fe34cd3863dc906dfe01d22d488f6eaf
Evan <evturn@gmail.com>
2014-12-14 23:14:31 -0500

    defaults added for round model

--- app/assets/javascripts/backbone/models/round.js
+++ app/assets/javascripts/backbone/models/round.js
@@ -1,3 +1,12 @@
 console.log('we got round')
 
-var Round = Backbone.Model.extend({});
+var Round = Backbone.Model.extend({
+	defaults: {
+		sex: "",
+		lbs: "",
+		hours: "",
+		drinks: "",
+		bac: "",
+		abv: ""
+	}
+});
--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,25 +3,25 @@ class BeersController < ApplicationController
   def index
 
     trend_url = "http://api.untappd.com/v4/beer/trending?client_id=#{ENV['UNTAPPD_ID']}&client_secret=#{ENV['UNTAPPD_SECRET']}" 
-    trending    = HTTParty.get( trend_url )
-    @featured    = trending['response']["micro"]["items"].sample
+    # trending  = HTTParty.get( trend_url )
+    # @featured = trending['response']["micro"]["items"].sample
 
-    respond_to do |f|
-      f.html { @top_beers }
-      f.json { render json: @featured }
-    end
+    # respond_to do |f|
+    #   f.html { @top_beers }
+    #   f.json { render json: @featured }
+    # end
 
-    @top_beers = trending['response']["micro"]["items"].map { |item| item }
+    # @top_beers = trending['response']["micro"]["items"].map { |item| item }
         
   end
 
   def search
 
-    query = params['query'].gsub(' ', '+')
-    url = "https://api.untappd.com/v4/search/beer?client_id=#{ENV['UNTAPPD_ID']}&client_secret=#{ENV['UNTAPPD_SECRET']}&q=#{query}"
-    response     = HTTParty.get( url )
-    beer         = response['response']['beers']['items'].first['beer']
-    render json: beer.to_json, status: 200
+    # query = params['query'].gsub(' ', '+')
+    # url = "https://api.untappd.com/v4/search/beer?client_id=#{ENV['UNTAPPD_ID']}&client_secret=#{ENV['UNTAPPD_SECRET']}&q=#{query}"
+    # # response = HTTParty.get( url )
+    # beer     = response['response']['beers']['items'].first['beer']
+    # render json: beer.to_json, status: 200
   end
 
 end
