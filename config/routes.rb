429bb1cff57b6da4ad8570608b4fc2550ea021f3
Evan <evturn@gmail.com>
2014-12-15 10:31:54 -0500

    unused routes deleted from rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root            to: "beers#index"
   get  '/beers'    => "beers#search"
   post '/rounds'   => "rounds#create"
-  get  '/users'    => "users#login"
 
 end
