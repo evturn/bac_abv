29d37930ee9c522b23956ff138a11034a5f3db95
Evan <evturn@gmail.com>
2014-12-15 10:34:25 -0500

    routes updated to not be prone to ruby interpolation with double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   
-  root            to: "beers#index"
-  get  '/beers'    => "beers#search"
-  post '/rounds'   => "rounds#create"
-
+  root           to: 'beers#index'
+  get  '/beers'  =>  'beers#search'
+  post '/rounds' =>  'rounds#create' 
 end
