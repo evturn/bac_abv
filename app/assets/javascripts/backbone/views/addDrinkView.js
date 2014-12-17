637104e5e2efac2446bc96c8a741b3921bee7242
Evan Turner <evturn@gmail.com>
2014-12-17 16:53:20 -0500

    need to fix the #add-drink button doing a submit post request to /rounds

--- app/assets/javascripts/backbone/views/addDrinkView.js
+++ app/assets/javascripts/backbone/views/addDrinkView.js
@@ -1,11 +1,11 @@
 console.log('we got addDrinkView')
 
 var AddDrinkView = Backbone.View.extend({
+	el: '#add-drink',
 	template: _.template($('#add-drink-template').html()),
 	initialize: function() {
 		this.listenTo("click #add-drink-button", 'change', this.render);
 	},
-	url: '/rounds',
 	render: function() {
 		this.$el.html(this.template);
 		$('#add-drink').append(this.$el);
